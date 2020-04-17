9aa8a263b54f3e121591f26dc1eca4c261d8844b
Arka Mondal <arkamondal1995@outlook.com>
2020-04-17 16:05:28 +0530

    17/04/2020

--- app/src/main/java/com/example/recyclerview_full/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_full/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.recyclerview_full
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView : RecyclerView
+    private lateinit var recyclerView: RecyclerView
+    private var animals: ArrayList<String> = ArrayList()
     private lateinit var recyclerAdapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val animals : ArrayList<String> = ArrayList()
         recyclerView = findViewById(R.id.recyclerView)
-        recyclerAdapter = RecyclerAdapter(animals)
 
         animals.add("cat")
         animals.add("owl")
@@ -47,5 +55,151 @@ class MainActivity : AppCompatActivity() {
         animals.add("monkey")
         animals.add("penguin")
         animals.add("parrot")
+
+        // To show the devider into the recyclerView this line is used
+        val dividerItemDecoration: DividerItemDecoration =
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        recyclerView.addItemDecoration(dividerItemDecoration)
+        recyclerAdapter = RecyclerAdapter(animals)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerAdapter(animals)
+
+        // This line add the itemtouch helper into the recyclerView
+        val itemTouchHelper: ItemTouchHelper = ItemTouchHelper(itemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
+
+    lateinit var deleteData: String
+
+    // Hare i implement the swipe delete futures
+    val itemTouchCallback: ItemTouchHelper.SimpleCallback =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                viewHolder1: RecyclerView.ViewHolder
+            ): Boolean {
+                //2 Return false in onMove. You don’t want to perform any special behavior here
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
+                //3 Call onSwiped when you swipe an item in the direction specified in the ItemTouchHelper. Here, you request the viewHolder parameter passed for the position of the item view, and then you remove that item from your list of photos. Finally, you inform the RecyclerView adapter that an item has been removed at a specific position.
+                // This line holde the position on adapter
+                val position: Int = viewHolder.adapterPosition
+
+                when (swipeDir) {
+                    ItemTouchHelper.LEFT -> {
+                        deleteData = animals.get(position)
+                        animals.removeAt(position)
+                        recyclerView.adapter?.notifyItemRemoved(position)
+                        // in this stape we implement the undo button in to the recyclerView/ This snackbar implement from meterial
+                        Snackbar.make(recyclerView, deleteData, Snackbar.LENGTH_LONG)
+                            .setAction("undo", View.OnClickListener {
+                                animals.add(position, deleteData)
+                                recyclerView.adapter?.notifyItemChanged(position)
+                            }).show()
+                    }
+
+                    ItemTouchHelper.RIGHT -> {
+                        // For the right swipe we would  flow this stape
+                        // step1 - make a list in out side the function
+                        // step2 - val data : String  =  animals.get(position)
+                        // step3 - this data into the created list
+                        // step4 - val archiveData : List<String> = ArrayList()
+                        // step5 - archiveData.add(data)
+                        /*
+                        * animals.removeAt(position)
+                        recyclerView.adapter?.notifyItemRemoved(position)
+
+                        // in this stape we implement the undo button in to the recyclerView/ This snackbar implement from meterial
+
+                        Snackbar.make(recyclerView, deleteData, Snackbar.LENGTH_LONG).setAction("Archived", View.OnClickListener {
+                            archiveData.removed(archiveData.lastIndexof(data)
+                            animals.add(position, data)
+                            recyclerView.adapter?.notifyItemChanged(position)
+                        }).show()
+
+                        * */
+
+                    }
+                }
+            }
+
+        }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+        val item: MenuItem = menu.findItem(R.id.search)
+        val searchView: SearchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                recyclerAdapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+
+        return true
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/recyclerview_full/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerview_full/RecyclerAdapter.kt
@@ -1,16 +1,21 @@
 package com.example.recyclerview_full
 
+import android.graphics.drawable.AnimationDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 
+
 class RecyclerAdapter(var animals: ArrayList<String>) :
-    RecyclerView.Adapter<RecyclerAdapter.animalViewHolder>() {
+    RecyclerView.Adapter<RecyclerAdapter.animalViewHolder>(), Filterable {
+
+    var animalsFull: ArrayList<String> = animals
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): animalViewHolder {
-        val item = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
-        return animalViewHolder(item)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
+        return animalViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
@@ -21,18 +26,65 @@ class RecyclerAdapter(var animals: ArrayList<String>) :
         holder.textView.text = animals[position]
         holder.rowCountTextView.text = position.toString()
 
+        val context = holder.mView.context
+        holder.mView.setOnClickListener {
+            Toast.makeText(context, animals.get(position), Toast.LENGTH_SHORT).show()
+        }
+        holder.mView.setOnLongClickListener {
+            Toast.makeText(context, "You click long " + animals.get(position), Toast.LENGTH_SHORT)
+                .show()
+            animals.remove(animals.get(position))
+            notifyDataSetChanged()
+            true
+        }
+        holder.imageView.setOnClickListener {
+            holder.wifiAnimation.isOneShot
+        }
     }
 
-    class animalViewHolder(item: View) : RecyclerView.ViewHolder(item) {
-        var textView: TextView
-        var rowCountTextView: TextView
+    class animalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var wifiAnimation: AnimationDrawable
+        var textView: TextView = itemView.findViewById(R.id.textView)
+        var rowCountTextView: TextView = itemView.findViewById(R.id.textView2)
+        var imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val mView: View = itemView
 
         init {
-            textView = item.findViewById(R.id.textView)
-            rowCountTextView = item.findViewById(R.id.textView2)
+            imageView.setBackgroundResource(R.drawable.animation)
+            wifiAnimation = imageView.background as AnimationDrawable
+        }
+    }
 
+    override fun getFilter(): Filter {
+        return datafilter
+    }
+
+    private var datafilter: Filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val filteredList: ArrayList<String> = ArrayList()
+            if (constraint.toString().isEmpty()) {
+                filteredList.addAll(animalsFull)
+            } else {
+                for (animal: String in animalsFull) {
+                    if (animal.toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        filteredList.add(animal)
+                    }
+                }
+            }
+            val filterResults: FilterResults = FilterResults()
+            filterResults.values = filteredList
+
+            return filterResults
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            animals.clear()
+            animals.addAll(results?.values as Collection<String>)
+            notifyDataSetChanged()
         }
 
     }
 
 }
+
+
